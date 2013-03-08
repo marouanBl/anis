94ad2f8ae40cb87ee5274daa820a8a9167e0d88b
marwenB <marwen.belghith@gmail.com>
2013-03-08 21:52:36 +0100

    FIRST COMMIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anis::Application.config.secret_token = 'cd1f34c16df01e20ccfa3fc7efbccf4d7a8db889685dfe4f3babde8190e605d2e89611706bdf623f1c4c97f9243909815a6f1845875c79049d5d60ca52dd6eee'
